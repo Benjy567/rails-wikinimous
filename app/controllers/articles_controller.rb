28105fa7f906bc0f88443b3615c5c7b70a2634ef
Benjamin Moreaux <benjy567@gmail.com>
2024-03-05 16:31:25 +0100

    exercice wikinimous non fini

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,46 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params_article)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(params_article)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, status: :see_other
+  end
+
+  private
+
+  def params_article
+    params.require(:article).permit(:title, :content)
+  end
+
+  #def set_restaurant
+   # @article = Article.find(params[:id])
+  #end
+end
